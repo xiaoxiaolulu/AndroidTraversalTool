d9e053d7de855ab88dca1efef9a4a91b875161b7
Null <546464268@qq.com>
2019-07-31 17:39:54 +0800

    测试

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 from lib.utils import get_devices
+from appium import webdriver
+from lib.public.parser import DumpParser
+from lib.public.logger import logger
+import selenium
 
-print(get_devices.capabilities())
+'''测试代码'''
+driver = webdriver.Remote('http://localhost:4723/wd/hub', get_devices.capabilities()[0])
+driver.implicitly_wait(15)
+D = DumpParser('uidump.xml')
+for value in D.get_element_path():
+    try:
+        driver.find_element_by_xpath(value).click()
+        logger.debug('Click element xpath is {}'.format(value))
+    except selenium.common.exceptions.NoSuchElementException:
+        logger.warning(value)
+        continue
